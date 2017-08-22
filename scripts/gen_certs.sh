b0a33624b4999f1b5514b2e93de06e086fcb69b5
Brandon Okert <brandon.okert@hootsuite.com>
2017-08-22 09:30:36 -0700

    Improved cert gen script to place them in the right place and auto encrypt, and cleaned up old uneeded secrets

--- scripts/gen_certs.sh
+++ scripts/gen_certs.sh
@@ -12,6 +12,8 @@ ETCD_PEER_NAME="etcd_peer"
 ETCD_CLIENT_NAME="etcd_client"
 KUBE_API_NAME="kube_api"
 KUBELET_NAME="kubelet"
+ADMIN_NAME="admin"
+DEVELOPER_NAME="developer"
 
 function parse_args {
     GETOPT=getopt
@@ -20,18 +22,21 @@ function parse_args {
         [[ ! -x $GETOPT ]] && (echo "you need gnu-getopt (brew)"; exit 1)
     fi
     local PARSED
-    PARSED=$($GETOPT --options d:e:s: --longoptions certs-dir:env:keysize: --name "$0" -- "$@")
+    PARSED=$($GETOPT --options d:p:e:s: --name "$0" -- "$@")
     eval set -- "$PARSED"
 
     while true; do
         case "$1" in
-            -d | --certs-dir) CERTS_DIR="$2";
+            -d) ANSIBLE_DIR="$2";
             shift 2 ;;
 
-            -e | --env) ENV="$2";
+            -p) ANSIBLE_VAULT_PASSWORD_FILE="$2";
             shift 2 ;;
 
-            -s | --keysize) KEYSIZE="$2";
+            -e) ENV="$2";
+            shift 2 ;;
+
+            -s) KEYSIZE="$2";
             shift 2 ;;
 
             --) shift;
@@ -43,14 +48,20 @@ function parse_args {
 
     # Required
     if [[ -z $ENV ]]; then
-        error "env" "must be set"
+        error "environment" "must be set"
+        usage
+        exit 1
+    fi
+
+    if [[ -z $ANSIBLE_VAULT_PASSWORD_FILE ]]; then
+        error "ansible-password-file" "must be set"
         usage
         exit 1
     fi
 
     # Defaults
-    if [[ -z $CERTS_DIR ]]; then
-        CERTS_DIR="generated_certs"
+    if [[ -z $ANSIBLE_DIR ]]; then
+        ANSIBLE_DIR="./ansible"
     fi
 
     if [[ -z $KEYSIZE ]]; then
@@ -73,8 +84,10 @@ Usage: $(basename "$0" [options])
   Generates new certs for each node-type.
 
   Etcd certs are always generated, even if etcd will not have its own physical node.
-  These certs need to be provisioned to the nodes after generating, or saved
-  somewhere for future use.
+  The certs are generated into the ansible provisioning roles, and the certs and keys
+  are auto encrypted. Thus they are ready to provision to nodes.
+  Also generated are the admin and developer account certs, which will need to
+  be saved for future distribution.
 
   The CA chain is:
     $ROOT_CA_NAME -> $ETCD_CA_NAME -> $ETCD_PEER_NAME
@@ -83,19 +96,28 @@ Usage: $(basename "$0" [options])
     $ROOT_CA_NAME -> $KUBE_CA_NAME -> $KUBE_API_NAME
     $ROOT_CA_NAME -> $KUBE_CA_NAME -> $KUBELET_NAME
 
+    $ROOT_CA_NAME -> $KUBE_CA_NAME -> $ADMIN_NAME
+    $ROOT_CA_NAME -> $KUBE_CA_NAME -> $DEVELOPER_NAME
+
 Options:
 
-  -e|--env 'production'              Environment to generate certs for. Determines
-  (required)                         the CN (Common Name) of each cert, as well
-                                     as the output subfolder.
+  (environment)
+  -e 'production'                    Environment to generate certs for. Determines
+  (required)                         the CN (Common Name) of each cert. When deploying
+                                     to different environemnts, the certs should always
+                                     be regenerated
 
-  -d|--certs-dir generated_certs     Folder to write new certs to. Defaults to
-                                     'generated_certs'. Will overwrite any existing
-                                     certs already in that folder. Certs are added
-                                     to subfolders within the certs-dir, one per
-                                     environment.
+  (ansible-password-file)
+  -p '~/ansible_password_file'       Path to the ansible password file for encrypting
+  (required)                         of certs and keys.
 
-  -s|--keysize 2048                  Size of the key in bits. Must be greater than
+  (ansible-directory)
+  -d './ansible'                     Folder where ansible roles-local is found.
+                                     Will overwrite any existing certs already in
+                                     the roles of that folder.
+
+  (keysize)
+  -s 2048                            Size of the key in bits. Must be greater than
                                      or equal to 1000.
 "
 
@@ -111,8 +133,9 @@ function info {
 }
 
 function gen_certs {
-    mkdir -p ${CERTS_DIR}/${ENV} > /dev/null
-    pushd ${CERTS_DIR}/${ENV} > /dev/null
+    info "Creating temporary certs dir. Do NOT commit this!"
+    mkdir -p .generated_certs/${ENV} > /dev/null
+    pushd .generated_certs/${ENV} > /dev/null
 
     info "Creating private Keys"
     set -x
@@ -123,6 +146,8 @@ function gen_certs {
     openssl genrsa -out ${ETCD_CLIENT_NAME}.key ${KEYSIZE}
     openssl genrsa -out ${KUBE_API_NAME}.key ${KEYSIZE}
     openssl genrsa -out ${KUBELET_NAME}.key ${KEYSIZE}
+    openssl genrsa -out ${ADMIN_NAME}.key ${KEYSIZE}
+    openssl genrsa -out ${DEVELOPER_NAME}.key ${KEYSIZE}
     set +x
 
     info "Creating Root CA cert"
@@ -132,17 +157,46 @@ function gen_certs {
     gen_signed_cert ${ETCD_CA_NAME} ${ROOT_CA_NAME}
     gen_signed_cert ${KUBE_CA_NAME} ${ROOT_CA_NAME}
 
-    info "Creating Node CA certs"
+    info "Creating Node certs"
     gen_signed_cert ${ETCD_PEER_NAME} ${ETCD_CA_NAME}
     gen_signed_cert ${ETCD_CLIENT_NAME} ${KUBE_CA_NAME}
     gen_signed_cert ${KUBE_API_NAME} ${KUBE_CA_NAME}
     gen_signed_cert ${KUBELET_NAME} ${KUBE_CA_NAME}
+
+    info "Creating User certs"
+    gen_signed_cert ${ADMIN_NAME} ${KUBE_CA_NAME}
+    gen_signed_cert ${DEVELOPER_NAME} ${KUBE_CA_NAME}
 
     info "Cleaning intermediate resources"
     rm -f *.srl
     rm -f *.csr
 
     popd > /dev/null
+
+    info "Moving and encrypting certs for kube-controller"
+    SSLROOT=${ANSIBLE_DIR}/roles-local/kube-controller/files/etc/ssl
+    mkdir -p ${SSLROOT}/certs > /dev/null
+    mkdir -p ${SSLROOT}/keys > /dev/null
+    for CERT in ${ETCD_CLIENT_NAME} ${ETCD_PEER_NAME} ${KUBE_API_NAME}; do
+      ansible-vault encrypt --vault-password-file ${ANSIBLE_VAULT_PASSWORD_FILE} --output ${SSLROOT}/certs/${CERT}.crt.vault .generated_certs/${ENV}/${CERT}.crt
+      ansible-vault encrypt --vault-password-file ${ANSIBLE_VAULT_PASSWORD_FILE} --output ${SSLROOT}/keys/${CERT}.key.vault .generated_certs/${ENV}/${CERT}.key
+    done
+
+    info "Moving and encrypting certs for kube-tls-node"
+    SSLROOT=${ANSIBLE_DIR}/roles-local/kube-tls-node/files/etc/ssl
+    mkdir -p ${SSLROOT}/certs > /dev/null
+    for CERT in ${ETCD_CA_NAME} ${KUBE_CA_NAME} ${ROOT_CA_NAME}; do
+      ansible-vault encrypt --vault-password-file ${ANSIBLE_VAULT_PASSWORD_FILE} --output ${SSLROOT}/certs/${CERT}.crt.vault .generated_certs/${ENV}/${CERT}.crt
+    done
+
+    info "Moving and encrypting certs for kubelet"
+    SSLROOT=${ANSIBLE_DIR}/roles-local/kubelet/files/etc/ssl
+    mkdir -p ${SSLROOT}/certs > /dev/null
+    mkdir -p ${SSLROOT}/keys > /dev/null
+    for CERT in ${KUBELET_NAME}; do
+      ansible-vault encrypt --vault-password-file ${ANSIBLE_VAULT_PASSWORD_FILE} --output ${SSLROOT}/certs/${CERT}.crt.vault .generated_certs/${ENV}/${CERT}.crt
+      ansible-vault encrypt --vault-password-file ${ANSIBLE_VAULT_PASSWORD_FILE} --output ${SSLROOT}/keys/${CERT}.key.vault .generated_certs/${ENV}/${CERT}.key
+    done
 }
 
 function gen_self_signed_cert {
@@ -160,7 +214,7 @@ function main {
     parse_args "$@"
 
     info "Generating certificate chain for '$ENV', using $KEYSIZE bit keys"
-    info "Output will be created or updated in ./$CERTS_DIR/$ENV/"
+    info "Encrypted output will be created in ./$ANSIBLE_DIR/"
     gen_certs
     exit $?
 }
